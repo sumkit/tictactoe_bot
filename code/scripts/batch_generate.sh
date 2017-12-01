eff204f8255408e23e1dc2740ac8a059645e3672
Srishti Srivastava <srishtis@latedays.andrew.cmu.edu>
2017-12-01 17:20:14 -0500

    hopefully working makefile

--- code/scripts/batch_generate.sh
+++ code/scripts/batch_generate.sh
@@ -2,13 +2,9 @@
 # generate jobs in batch
 
 threads=(1 236) # The number of threads 
-inputs=(timeinput/easy_1024.txt) # The name of the input files
 rm -f *.job
 
-for f in ${inputs[@]}
-do
-    for t in ${threads[@]}
-    do
-	../scripts/generate_jobs.sh $f $t
-    done
-done
+for t in ${threads[@]}
+  do
+    ../scripts/generate_jobs.sh $t
+  done
