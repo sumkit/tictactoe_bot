3d234cd106963f73801bb4383cb920fe08199e3d
Srishti Srivastava <srishtis@latedays.andrew.cmu.edu>
2017-12-08 09:48:02 -0500

    5x5

--- code/main.cpp
+++ code/main.cpp
@@ -12,7 +12,7 @@
 static int _argc;
 static const char **_argv;
 
-const int N = 3; //dimensions of board
+const int N = 5; //dimensions of board
 
 int min(int a, int b) {
   return (a < b) ? a : b;
@@ -155,7 +155,7 @@ bool isWinner(int row, int col, char* board, char player) {
 int* calculateSmallBoardScore(char* board, int row, int col, 
   char player, char opp, int* oldScore) {
 
-  int center = 1 * N + 1; 
+  int center = (N/2) * N + (N/2); 
   int* res = (int *) calloc(2, sizeof(int)); 
   
   int oldPlayerScore;
@@ -180,12 +180,11 @@ int* calculateSmallBoardScore(char* board, int row, int col,
   int iWon = 1; 
   int fail = 0;
 
-  //add 2 for getting a sequence of two tiles
-  //subtract 2 from the opponent for blocking their sequence of two tiles
+  //add 2 for getting a sequence of N-1 tiles
+  //subtract 2 from the opponent for blocking their sequence of N-1 tiles
 
-  bool diagOne = ((row*N+col) % 4 == 0); 
-  bool diagTwo = (((row*N+col) % 2 == 0) && ((row*N+col) > 0) && ((row*N+col) < N*N-1)); 
-
+  bool diagOne = (row==col);
+  bool diagTwo = (row == (N-1)-col);  
 
   for (int i = N*row; i < (N * (row + 1)); i++) { 
     if (i == row*N + col) 
@@ -194,8 +193,8 @@ int* calculateSmallBoardScore(char* board, int row, int col,
     if (board[i] == player) iWon += 1; 
   }
 
-  if (iWon == 2 && opWon == 0) myScore += 2; 
-  if (opWon == 2) hisScore -= 2; 
+  if (iWon == N-1 && opWon == 0) myScore += 2; 
+  if (opWon == N-1) hisScore -= 2; 
 
   opWon = 0;  
   iWon = 1; 
@@ -206,33 +205,34 @@ int* calculateSmallBoardScore(char* board, int row, int col,
     if (board[i] == player) iWon += 1;
   }
 
-  if (iWon == 2 && opWon == 0) myScore += 2; 
-  if (opWon == 2) hisScore -= 2; 
+  if (iWon == N-1 && opWon == 0) myScore += 2; 
+  if (opWon == N-1) hisScore -= 2; 
+
 
   if (diagOne) { 
-    opWon = 0;  
-    iWon = 1; 
-    for (int i = 0; i < N*N; i += 4) {
-      if (i == row*N+col) 
-        continue; 
-      if (board[i] == opp) opWon += 1; 
-      if (board[i] == player) iWon += 1;
-    }
-    if (iWon == 2 && opWon == 0) myScore += 2; 
-    if (opWon == 2) hisScore -= 2; 
+    opWon = 0;
+    iWon = 0; 
+    for (int i = 0; i < N; i++) { 
+      int idx = i * N + i;
+      if (board[idx] == opp) opWon += 1; 
+      if (board[idx] == player) iWon += 1;
+    } 
   }
+  if (iWon == N-1 && opWon == 0) myScore += 2; 
+  if (opWon == N-1) hisScore -= 2; 
+
+
 
   if (diagTwo) { 
     opWon = 0;  
-    iWon = 1; 
-    for (int i = 2; i < N*N-1; i += 2) {
-      if (i == row*N+col) 
-        continue; 
-      if (board[i] == opp) opWon += 1; 
-      if (board[i] == player) iWon += 1;
+    iWon = 0; 
+    for (int i = 0; i < N; i++) {
+      int idx = i * N + (N-1-i); 
+      if (board[idx] == opp) opWon += 1; 
+      if (board[idx] == player) iWon += 1;
     }
-    if (iWon == 2 && opWon == 0) myScore += 2; 
-    if (opWon == 2) hisScore -= 2; 
+    if (iWon == N-1 && opWon == 0) myScore += 2; 
+    if (opWon == N-1) hisScore -= 2; 
   }
 
 
